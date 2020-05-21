78c19e16702b07d0a81c03d903a9de390ccb4dda
Andy <andrii.pylypenko@recoded.com.ua>
2020-05-21 06:49:18 -0700

    Add project files.

---
+++ TestSiteMapAnalysis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestSiteMapAnalysis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
